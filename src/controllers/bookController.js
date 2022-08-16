06a6abe1c4a3967c77fb1c55d1c79b3a3f6ba04b
RaviiSharma <engineerravi036@gmail.com>
2022-08-17 00:31:11 +0530

    Assignment of mongo-session-part1

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,16 +1,44 @@
+const { count } = require("console")
 const BookModel= require("../models/bookModel")
 
 const createBook= async function (req, res) {
-    let data= req.body
-
-    let savedData= await BookModel.create(data)
-    res.send({msg: savedData})
+    let bookData= req.body
+    let book= await BookModel.create(bookData)
+    res.send({msg: book})
 }
 
-const getBooksData= async function (req, res) {
-    let allBooks= await BookModel.find(  { authorName : "SK" , isPublished: true }  )
-    res.send({msg: allBooks})
+const getBook= async function (req,res){
+    let book=await BookModel.find().select({bookName:1,authorName:1,_id:0})
+    res.send({msg: book})
 }
+
+const bookInYear = async function(req,res){
+    let year=req.body.year
+    let book=await BookModel.find({year:2022})
+    res.send({msg:book})
+}
+
+const particularBooks= async function(req,res){
+    let obj=req.body
+    let book = await BookModel.find(obj)
+    res.send({msg: book})
+}
+
+const getINRBooks= async function(req,res){
+    let book = await BookModel.find({"prices.indianPrice":{$in:[100,200,500]}})
+    res.send({msg: book})
+}
+
+const getRandomBooks = async function(req,res){
+    let book=await BookModel.find({$or:[{totalPages:{$gt:500}},{stockAvailable: {$eq:true}}]})
+    res.send({msg: book})
+}
+
+
 
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+module.exports.getBook= getBook
+module.exports.bookInYear= bookInYear
+module.exports.particularBooks= particularBooks
+module.exports.getINRBooks= getINRBooks
+module.exports.getRandomBooks= getRandomBooks
